e67f510e39ba21ad22f757b17d1394b590297d28
owenprotz <owen.protz@stu.ocsb.ca>
2022-11-15 14:04:25 -0500

    chicken 2

--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,10 +1,21 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
-        System.out.println("Daily Average:   " +dailyAverage);
-        System.out.println("Monthly Average: " +monthlyAverage);
-        System.out.println("Monthly Profit:  $" +monthlyProfit);
+    	double dailyAverage;
+    	double monthlyAverage;
+    	double monthlyProfit;
+    	int totalEggs = 0;
+    	//Monday +100 eggs 
+    	totalEggs+=100;
+    	//Tuesday
+    	totalEggs+=121;
+    	totalEggs+=117;
+    	dailyAverage=totalEggs/3;
+    	monthlyAverage=dailyAverage*30;
+    	monthlyProfit = monthlyAverage *0.18;
+    	System.out.println("Daily Average:  " +dailyAverage);
+    	System.out.println("Monthly Average:  " +monthlyAverage);
+    	System.out.println("Monthly Profit:  " +monthlyProfit);
     }
     
 }
